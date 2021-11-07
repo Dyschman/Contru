e8fca1e17bea683741e2b70b5b59933672fb1113
Dyschman <dyschman@gmail.com>
2021-11-07 19:16:21 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i + "");
+        }
+
+
+        for (int i = 6; i >= -6; i -= 2) {
+            System.out.println(i + "");
+        }
+
+        int sum = 0;
+        int i = 10;
+        do {
+            if (i%2!=0){
+                sum+=i;
+            }
+            i++;
+        } while (i!=20);
+        System.out.print("Сумма нечетных чисел = "+sum);
+
+
+    }
+}
